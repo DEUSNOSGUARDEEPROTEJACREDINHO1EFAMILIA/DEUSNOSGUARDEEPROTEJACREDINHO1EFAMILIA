a805b9f7583f53f06ef3d88ed6843cf7d1d9bc62
DEUSNOSGUARDEEPROTEJACREDINHO1EFAMILIA <144938507+DEUSNOSGUARDEEPROTEJACREDINHO1EFAMILIA@users.noreply.github.com>
2023-09-13 13:38:32 -0300

    Create BOASVINDAS.VIDA
    +positivo energias👋 "O Senhor te abençoe e te guarde; o Senhor faça resplandece 
    o seu rosto sobre ti
    e te conceda graça; o Senhor volte para ti o seu rosto
    e te dê FAMÍLIA DE TANCREDO TIMÃO SOUZA SANTOS O CREDINHO 01 TUDO DE BOM DA PARTE DO DEUS VIVO QUE CRIOU OS CÉUS E TERRA E TERRA 🌎🌍 DE A CREDINHO O TANCREDO TIMÃO SOUZA SANTOS E FAMILIARES QUE GOSTAM DELE E QUER VER SEU BEM ASSIM COMO A SUA MÃE TUDO DE BOM
    A graça do Senhor Jesus Cristo CREDINHO1TANCREDO TIMÃO SOUZA SANTOS, o amor de Deus e a comunhão do Espírito Santo sejam com todos vocês.
    2 Coríntios 13:14
    - 📫BEMVINDO
    <!---
    DEUSNOSGUARDEEPROTEJACREDINHO1EFAMILIA/DEUSNOSGUARDEEPROTEJACREDINHO1EFAMILIA MUITA LUZ EM SUAS VIDAS COM BOMDIA E BOA NOITE✨ Especial ✨ beLA HISTÓRIA  DE VIDA (WORLD4CANTOSDOWORLD) on  GitHub SCRIPT VIDA BOA.
    --->

---
+++ README.m
@@ -0,0 +1,10 @@
++positivo energias👋 "O Senhor te abençoe e te guarde; o Senhor faça resplandece 
+o seu rosto sobre ti
+e te conceda graça; o Senhor volte para ti o seu rosto
+e te dê FAMÍLIA DE TANCREDO TIMÃO SOUZA SANTOS O CREDINHO 01 TUDO DE BOM DA PARTE DO DEUS VIVO QUE CRIOU OS CÉUS E TERRA E TERRA 🌎🌍 DE A CREDINHO O TANCREDO TIMÃO SOUZA SANTOS E FAMILIARES QUE GOSTAM DELE E QUER VER SEU BEM ASSIM COMO A SUA MÃE TUDO DE BOM
+A graça do Senhor Jesus Cristo CREDINHO1TANCREDO TIMÃO SOUZA SANTOS, o amor de Deus e a comunhão do Espírito Santo sejam com todos vocês.
+2 Coríntios 13:14
+- 📫BEMVINDO
+<!---
+DEUSNOSGUARDEEPROTEJACREDINHO1EFAMILIA/DEUSNOSGUARDEEPROTEJACREDINHO1EFAMILIA MUITA LUZ EM SUAS VIDAS COM BOMDIA E BOA NOITE✨ Especial ✨ beLA HISTÓRIA  DE VIDA (WORLD4CANTOSDOWORLD) on  GitHub SCRIPT VIDA BOA.
+--->
